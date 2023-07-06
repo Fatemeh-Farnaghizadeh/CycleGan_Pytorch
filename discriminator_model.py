06f7985f20411b9d22d178c553983ad3d1c018ea
fatemeh <fatemeh@gmail.com>
2023-07-07 00:33:31 +0330

    all complete

--- discriminator_model.py
+++ discriminator_model.py
@@ -1,52 +1,73 @@
 import torch
-from torch import nn
+import torch.nn as nn
+
 
 class Block(nn.Module):
-
-    def __init__(self, in_channels, out_channels, kernel_size=4, stride=2, bias=True, padding=1, padding_mode='reflect'):
+    def __init__(self, in_channels, out_channels, stride):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, padding_mode=padding_mode),
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                4,
+                stride,
+                1,
+                bias=True,
+                padding_mode="reflect",
+            ),
             nn.InstanceNorm2d(out_channels),
-            nn.LeakyReLU(0.2)
+            nn.LeakyReLU(0.2, inplace=True),
         )
 
     def forward(self, x):
         return self.conv(x)
-    
+
 
 class Discriminator(nn.Module):
-
-    def __init__(self, in_channels, features=[64, 128, 256, 512]):
+    def __init__(self, in_channels=3, features=[64, 128, 256, 512]):
         super().__init__()
         self.initial = nn.Sequential(
-            nn.Conv2d(in_channels, features[0], kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
-            nn.LeakyReLU(0.2)
+            nn.Conv2d(
+                in_channels,
+                features[0],
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                padding_mode="reflect",
+            ),
+            nn.LeakyReLU(0.2, inplace=True),
         )
-        in_channels=64
-        layers = []
 
+        layers = []
+        in_channels = features[0]
         for feature in features[1:]:
             layers.append(
-                Block(in_channels, feature, stride=1 if feature==features[-1] else 2)
+                Block(in_channels, feature, stride=1 if feature == features[-1] else 2)
             )
             in_channels = feature
-
         layers.append(
-            nn.Conv2d(in_channels, 1, kernel_size=4, stride=1, padding=1, padding_mode='reflect')
+            nn.Conv2d(
+                in_channels,
+                1,
+                kernel_size=4,
+                stride=1,
+                padding=1,
+                padding_mode="reflect",
+            )
         )
-
-        self. model = nn.Sequential(*layers)
+        self.model = nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.initial(x)
-        return torch.sigmoid(self.model(x)
-    )
+        return torch.sigmoid(self.model(x))
+
+
 def test():
     x = torch.randn((5, 3, 256, 256))
-    model = Discriminator(3)
-    pred = model(x)
-    print(pred.shape)
+    model = Discriminator(in_channels=3)
+    preds = model(x)
+    print(preds.shape)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test()
