a2d9e073a2248315c58aab3352edcffd2fc182c0
fatemeh <fatemeh@gmail.com>
2023-07-07 11:02:16 +0330

    final1

--- utils.py
+++ utils.py
@@ -18,11 +18,9 @@ def load_checkpoint(checkpoint_file, model, optimizer, lr):
     model.load_state_dict(checkpoint["state_dict"])
     optimizer.load_state_dict(checkpoint["optimizer"])
 
-    # If we don't do this then it will just have learning rate of old checkpoint
-    # and it will lead to many hours of debugging \:
     for param_group in optimizer.param_groups:
         param_group["lr"] = lr
 
 
 
-        
+
